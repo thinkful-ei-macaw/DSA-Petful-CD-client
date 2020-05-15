cc17888bdeab9b107777b9c01e0f250a79cd3eb8
Connor McCormack <mccormack.connor.m@gmail.com>
2020-05-15 13:19:20 -0500

    react error

---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class LandingPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Description of Adoption Process</p>
+        <img src='https://cdn.abcotvs.com/dip/images/5760725_dogadoption.JPG?w=1600' alt='really cute dog getting adopted'/>
+        <Link to={`/AdoptionPage`}>See available pets!</Link>
+      </div>
+    )
+  }
+}
+export default LandingPage
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { React, Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import LandingPage from '../components/LandingPage'
 
-function Root() {
-  return <div>
-    <h1>Petful</h1>
-  </div>
+
+class Root extends Component {
+  
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>Petful</h1>
+        <Switch>
+          <Route exact path={'/'} component={LandingPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default Root
